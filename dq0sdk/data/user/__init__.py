7d3419948e1b6e5b52436a4778698ebc7e8ac6d5
Jona Boeddinghaus <jb@gradient0.com>
2020-02-10 13:59:27 +0100

    Added user data source template

---
+++ dq0sdk/data/user/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""DQ0 SDK User Data Sources package.
+
+This package contains templates for user defined data sources.
+"""
+
+from .user_source import UserSource
+
+__all__ = [
+    'UserSource'
+]
