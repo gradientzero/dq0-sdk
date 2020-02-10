f10a52185fad52c6178c46b688b79a716e77a7b8
Jona Boeddinghaus <jb@gradient0.com>
2020-02-10 15:10:07 +0100

    user data import fix

--- dq0sdk/data/__init__.py
+++ dq0sdk/data/__init__.py
@@ -5,15 +5,16 @@ This package contains the data connector abstract classes and
 implementing subclasses.
 """
 
-from . import adult, csv
+from . import adult, csv, user
 from .connector import Connector
 from .source import Source
 
 __all__ = [
     'Connector',
     'Source',
-    'csv',
     'adult',
+    'csv',
+    'user',
     'census_data',
     '20_newsgroups',
     'cifar10',
