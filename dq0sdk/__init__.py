c15a8f874b268bd0cbfd9bdd976b244b9e252add
Artur Susdorf <artur.susdorf@gmail.com>
2020-03-23 17:30:08 +0100

    resolved pytest error

--- dq0sdk/__init__.py
+++ dq0sdk/__init__.py
@@ -4,16 +4,17 @@
 This is the main dq0sdk package containing everything SDK related
 """
 
+import pkg_resources  # part of setuptools
+
 from . import data
 from . import models
+
+# dq0 sdk version
+version = pkg_resources.require("dq0sdk")[0].version
+del pkg_resources
 
 __all__ = [
     'version',
     'data',
     'models'
 ]
-
-# dq0 sdk version
-import pkg_resources  # part of setuptools
-version = pkg_resources.require("dq0sdk")[0].version
-del pkg_resources
