c56c53dea85f3f808202b7fd17844670ae35b13f
Jona Boeddinghaus <jb@gradient0.com>
2020-04-14 06:20:49 +0200

    fixed dynamic module import

--- dq0sdk/data/user/__init__.py
+++ dq0sdk/data/user/__init__.py
@@ -9,14 +9,13 @@ import importlib
 
 test_case = 'Census'  # 'Cifar_10' '20_Newsgroups' 'Census'
 
-module = None
+UserSource = None
 if test_case.lower() == 'Cifar_10'.lower():
-    module = importlib.import_module('dq0sdk.examples.cifar.data.user_source')
+    UserSource = getattr(importlib.import_module('dq0sdk.examples.cifar.data.user_source'), 'UserSource')
 elif test_case.lower() == '20_Newsgroups'.lower():
-    module = importlib.import_module('dq0sdk.examples.newsgroups.data.user_source')
+    UserSource = getattr(importlib.import_module('dq0sdk.examples.newsgroups.data.user_source'), 'UserSource')
 elif test_case.lower() == 'Census'.lower():
-    module = importlib.import_module('dq0sdk.examples.census.data.user_source_for_bayesian_model')
-from module import UserSource  # noqa: E402
+    UserSource = getattr(importlib.import_module('dq0sdk.examples.census.data.user_source_for_bayesian_model'), 'UserSource')
 
 __all__ = [
     'UserSource'
--- dq0sdk/models/user/__init__.py
+++ dq0sdk/models/user/__init__.py
@@ -8,14 +8,13 @@ import importlib
 
 test_case = 'Census'  # 'Cifar_10' '20_Newsgroups' 'Census'
 
-module = None
+UserModel = None
 if test_case.lower() == 'Cifar_10'.lower():
-    module = importlib.import_module('dq0sdk.examples.cifar.model.user_model')
+    UserModel = getattr(importlib.import_module('dq0sdk.examples.cifar.model.user_model'), 'UserModel')
 elif test_case.lower() == '20_Newsgroups'.lower():
-    module = importlib.import_module('dq0sdk.examples.newsgroups.model.user_model')
+    UserModel = getattr(importlib.import_module('dq0sdk.examples.newsgroups.model.user_model'), 'UserModel')
 elif test_case.lower() == 'Census'.lower():
-    module = importlib.import_module('dq0sdk.examples.census.model.bayesian_user_model')
-from module import UserModel  # noqa: E204
+    UserModel = getattr(importlib.import_module('dq0sdk.examples.census.model.bayesian_user_model'), 'UserModel')
 
 # from .user_model import UserModel
 
