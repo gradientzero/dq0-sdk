b5ea94ff034b26240b64b7be16ea27b2a1a7f602
Artur Susdorf <artur.susdorf@gmail.com>
2020-07-16 11:57:59 +0200

    moved notebooks out of cli; splitted requirements(+dev)

--- DEVNOTES.md
+++ DEVNOTES.md
@@ -1,17 +1,17 @@
-
 ## Dev Setup
 
 Create a suitable virtual environment:
 
 ```bash
-conda create -n dq0-sdk python=3.6
-source activate dq0-sdk
+conda create -n dq0 python=3.7
+source activate dq0
 ```
 
 And install dependencies
 
 ```bash
 pip install -r requirements.txt
+pip install -r requirements-dev.txt
 ```
 
 ## Run tests
@@ -41,32 +41,15 @@ generate pdf:
 ```
 
 
-
-## Dev Guide
-
-This repository contains production-grade software. Thus, strict enterprise software development guidelines are to be respected.
-The git workflow looks like this:
-
-![git workflow](git.png)
-
-Feature branches are merged to the development branch via pull requests and prior code review.
-Continuous integration is implemented via github actions for both development and master branch.
-
-### Code style and documentation
-
-Python code styles follows the PEP 8 style guide and is inforced by flake8. CI builds will fail if flake8 produces errors.
-
-Code documentation follows the python google docstring format: [https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html](https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html)
-
-Code documentation is generated with the included sphinx library and makefile in the doc subdirectory.
-
-
 ## Installation
 
 Install in setup tools development mode:
 
 ```bash
 pip install -e git+https://github.com/gradientzero/dq0-sdk.git
+
+# or from source
+pip install .
 ````
 
 Production / binary installation TBD
@@ -76,6 +59,3 @@ Production / binary installation TBD
 python setup.py sdist bdist_wheel
 ```
 it will create at least two files in "dist".
-
-## Usage
-for a detailed description of how to setup, train and test and new mode see [dq0sdk/examples/yaml/readme.md](dq0sdk/examples/yaml/readme.md)
--- setup.py
+++ setup.py
@@ -5,30 +5,57 @@ Copyright 2020, Gradient Zero
 All rights reserved
 """
 
+import os
 from setuptools import find_packages, setup
+from setuptools_cythonize import get_cmdclass
 
 
-# Version
+# meta information
 VERSION = '1.1.0'
+NAME = 'dq0sdk'
+DESCRIPTION = 'DQ0 SDK Runtime'
+
+# Get the long description from the README file
+LONG_DESCRIPTION = ''
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
+    LONG_DESCRIPTION = f.read()
+LONG_DESCRIPTION_CONTENT_TYPE = 'text/markdown'
+
+AUTHOR = 'Gradient Zero',
+AUTHOR_EMAIL = 'dq0@gradient0.com',
+URL = 'https://www.dq0.io'
+SETUP_REQUIRES = [
+    'setuptools>=41.0.0',
+    'pytest-runner~=5.2',
+    'flake8~=3.8.1',
+    'flake8-import-order~=0.18.1',
+]
+
+PACKAGES = find_packages()
 
 # Requirements
-with open("requirements.txt") as f:
-    install_requires = [line for line in f.read().splitlines()
-                        if line and not line.startswith("#")]
+INSTALL_REQUIRES = []
+with open('requirements.txt') as f:
+    INSTALL_REQUIRES = [line for line in f.read().splitlines()
+                        if line and not line.startswith('#')]
+
+ENTRY_POINTS = {}
+
 
 # Setup
 setup(
-    name="dq0sdk",
+    cmdclass=get_cmdclass(),
+    name=NAME,
     version=VERSION,
-    description="DQ0 SDK",
-    author="Gradient Zero",
-    author_email="dq0@gradient0.com",
-    packages=find_packages(),
-    install_requires=install_requires,
-    setup_requires=[
-        "setuptools>=41.0.0",
-        "pytest-runner~=5.2",
-        "flake8~=3.8.1",
-        "flake8-import-order~=0.18.1"
-    ]
+    entry_points=ENTRY_POINTS,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    packages=PACKAGES,
+    install_requires=INSTALL_REQUIRES,
+    setup_requires=SETUP_REQUIRES,
 )
+
