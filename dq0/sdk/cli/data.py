215dc82564ba344a4d7e47a3f41b4a0478e99551
Dominic Viehböck <dominic.viehbock@gmail.com>
2020-11-19 16:24:11 +0100

    description update

--- dq0/sdk/cli/data.py
+++ dq0/sdk/cli/data.py
@@ -15,10 +15,14 @@ from dq0.sdk.cli.runner import QueryRunner
 class Data:
     """A data source wrapper
 
-    Provides methods to call data info jobs
+    Provides an interface for inspecting/interacting with data sources as well as query methods
 
     Example:
         >>> # get data source
+        >>> # multiple ways of instantiating a Data object, e.g. over it's UUID/Name or via a dict representing it:
+        >>> data = Data('data_name')
+        >>> data = Data('data_uuid', project=project)
+        >>> data = Data({'data_uuid': 'uuid', 'data_name': 'name', 'data_type': 'type'})
         >>> data = project.get_available_data_sources()[0] # doctest: +SKIP
         >>>
         >>>  # call dp mean
@@ -26,6 +30,9 @@ class Data:
         >>>
         >>>  # call with where clause
         >>>  result = data.mean(cols=['age'], query="where age > 30 and age < 40") # doctest: +SKIP
+        >>>
+        >>>  # run a query on this data source
+        >>>  result = data.query("select a.* from table a where age > 30 and age < 40") # doctest: +SKIP
 
     Args:
         project (:obj:`dq0.sdk.cli.Project`): The project
